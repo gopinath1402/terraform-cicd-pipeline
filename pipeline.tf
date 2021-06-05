8a994d12da441c7cc0e7e7b15ae1d81067d14702
Gopinath Perumal <gopinath.perumal@onmobile.com>
2021-06-05 19:43:01 +0530

    app cicd update

--- pipeline.tf
+++ pipeline.tf
@@ -122,18 +122,13 @@ resource "aws_codebuild_project" "app-image-build" {
   service_role = aws_iam_role.tf-codebuild-role.arn
 
   artifacts {
-    type = "CODEPIPELINE"
+    type = "NO_ARTIFACTS"
   }
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
     image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "SERVICE_ROLE"
-    registry_credential {
-      credential          = var.dockerhub_credentials
-      credential_provider = "SECRETS_MANAGER"
-    }
   }
   source {
     type      = "CODEPIPELINE"
