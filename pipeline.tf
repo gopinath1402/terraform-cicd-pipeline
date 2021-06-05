e01136f944dba1db78a3d9ffbda0ab331f4921f0
Gopinath Perumal <gopinath.perumal@onmobile.com>
2021-06-05 21:10:15 +0530

    code build docker image changes

--- pipeline.tf
+++ pipeline.tf
@@ -127,12 +127,36 @@ resource "aws_codebuild_project" "app-image-build" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:4.0"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
     type                        = "LINUX_CONTAINER"
   }
   source {
     type      = "CODEPIPELINE"
-    buildspec = file("buildspec/application-buildspec.yml")
+  }
+}
+
+resource "aws_codebuild_project" "application-apply" {
+  name         = "app-cicd-apply"
+  description  = "Apply stage for terraform"
+  service_role = aws_iam_role.tf-codebuild-role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "hashicorp/terraform:0.14.3"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+    registry_credential {
+      credential          = var.dockerhub_credentials
+      credential_provider = "SECRETS_MANAGER"
+    }
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = file("buildspec/apply-buildspec.yml")
   }
 }
 
@@ -183,7 +207,7 @@ resource "aws_codepipeline" "app_cicd_pipeline" {
     name = "Deploy"
     action {
       name            = "Deploy"
-      category        = "Deploy"
+      category        = "Build"
       provider        = "CodeBuild"
       version         = "1"
       owner           = "AWS"
